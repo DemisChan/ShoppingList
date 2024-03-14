ed35d17c866bf73ece2e96df270fb832c5212300
Demis Lavrentidis <demis.lavrentidis@atombank.co.uk>
2024-03-14 13:42:11 +0000

    Created MainViewModel.kt and methods to interact with domain layer

--- app/src/main/java/com/udemyAndroid/shoppinglist/presentation/MainActivity.kt
+++ app/src/main/java/com/udemyAndroid/shoppinglist/presentation/MainActivity.kt
@@ -1,17 +1,30 @@
 package com.udemyAndroid.shoppinglist.presentation
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.ViewModelProvider
 import com.udemyAndroid.shoppinglist.R
+import com.udemyAndroid.shoppinglist.domain.ShopItem
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: MainViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        viewModel = ViewModelProvider(this)[MainViewModel::class.java]
+        viewModel.shopList.observe(this) {
+            Log.d("Main Activity Test", it.toString())
+        }
+        viewModel.getShopList()
+        viewModel.deleteShopList(ShopItem("Name 3", 3, true, 3))
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
