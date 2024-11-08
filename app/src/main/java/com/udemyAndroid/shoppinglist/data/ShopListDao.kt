bed4c2d41916b49664bc5775ebae41ab71c5db76
Demis Lavrentidis <demis.lavrentidis@atombank.co.uk>
2024-11-08 11:37:38 +0000

    * Created Dao object, Database, and ShopItemDbModel for Shoplist app

---
+++ app/src/main/java/com/udemyAndroid/shoppinglist/data/ShopListDao.kt
@@ -0,0 +1,26 @@
+package com.udemyAndroid.shoppinglist.data
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Query
+import androidx.room.Upsert
+
+@Dao
+interface ShopListDao {
+
+    @Query("SELECT * FROM shop_items")
+    fun getShopList(): LiveData<List<ShopItemDbModel>>
+
+    @Upsert
+    fun addShopItem(shopItemDbModel: ShopItemDbModel)
+
+    @Delete
+    fun deleteShopItem(shopItemId: Int)
+
+    @Query("SELECT * FROM shop_items WHERE id=:shopItemId LIMIT 1")
+    fun getShopItem(shopItemId: Int): ShopItemDbModel
+
+
+
+}
